6747f16fb257182fb41a6ea407b367b4a64e0fd1
hubaiyin <1150122664@qq.com>
2023-09-10 22:17:46 +0800

    警报界面

---
+++ pages/sys/personal/personal.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		this is personal
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
