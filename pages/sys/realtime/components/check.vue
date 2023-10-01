e1d3bdb9c6b8aa4c384f5092c19234b65b4904f2
hubaiyin <1150122664@qq.com>
2023-10-01 18:14:21 +0800

    警报界面接口对接

---
+++ pages/sys/realtime/components/check.vue
@@ -0,0 +1,134 @@
+<template>
+  <view class="informBox">
+    <view class="video">
+      <video
+        :src="warnData.video"
+        style="width: 328px; height: 246px; transition: 1s"
+      ></video>
+    </view>
+    <view style="height: 200px; overflow: auto; width: 328px">
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 事件id </view>
+          <view class="column right">
+            {{ warnData.id }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 摄像头 </view>
+          <view class="column right">
+            {{ warnData.name }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 区域 </view>
+          <view class="column right">
+            {{ warnData.department }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 类别 </view>
+          <view class="column right">
+            {{ warnData.eventName }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 等级 </view>
+          <view class="column right"> {{ warnData.level }}级 </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 时间 </view>
+          <view class="column right" style="font-size: 32rpx">
+            {{ warnData.time }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 状态 </view>
+          <view
+            class="column right"
+            :class="warnData.deal === '已处理' ? 'dealt' : 'unDealt'"
+          >
+            {{ warnData.deal }}
+          </view>
+        </view>
+      </view>
+      <view class="contents">
+        <view class="line">
+          <view class="column left"> 处理内容 </view>
+          <view class="column right">
+            {{ warnData.content }}
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Check",
+  props: {
+    warnData: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.informBox {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .video {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 32rpx;
+    width: 328px;
+    height: 246px;
+  }
+  .contents {
+    width: 100%;
+    font-size: 38rpx;
+    .line {
+      width: 100%;
+      display: flex;
+      .column {
+        height: 100rpx;
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .left {
+        background-color: #e0e0e0;
+      }
+      .dealt {
+        color: #06bfa1;
+      }
+      .unDealt {
+        color: #ff5d5d;
+      }
+    }
+    .textarea {
+      border: 1px solid #b0b1b1;
+      border-radius: 15rpx;
+    }
+  }
+}
+</style>
---
+++ pages/sys/start/start.vue
@@ -0,0 +1,65 @@
+<template>
+	<view style="width: 100vw;height: 100vh;" class="img">
+		<image
+		  src="../../../static/startBack.png"
+		  mode="aspectFit"
+		></image>
+		<view class="text">
+			医患智警
+		</view>
+		<view class="mainTitle">
+			基于YOLOv8的医院危险事件智能警报系统
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@font-face{
+	font-family: YSBTH;
+	src: url(../../../static/fonts/YouSheBiaoTiHei-2.ttf);
+}
+.img{
+	position: relative;
+	image{
+		height: 100%;
+		width: 100%;
+	}
+	.text{
+		writing-mode: vertical-lr;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-54%);
+		color: #6C84C3;
+		font-size: 146rpx;
+		// font-weight: bold;
+		letter-spacing: 10rpx;
+		font-family: YSBTH;
+	}
+	.mainTitle{
+		color: #A8BAEA;
+		font-size: 28rpx;
+		position: absolute;
+		// height: 60rpx;
+		// padding-top: 20rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-family: YSBTH;
+		width: 250px;
+		left: 50%;
+		transform: translateX(-50%);
+		bottom: 4.5%;
+	}
+}
+</style>
